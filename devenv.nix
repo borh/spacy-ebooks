2adca4ebf617a688820039d512c8dbb8216815b2
Bor Hodošček <dev@bor.space>
2023-07-04 17:29:45 +0900

    feat: cuda support; test script

--- devenv.nix
+++ devenv.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
 {
   # https://devenv.sh/basics/
@@ -22,6 +22,29 @@
   };
 
   scripts.export-pip.exec = "poetry export -f requirements.txt --output requirements.txt; poetry export --with llm -f requirements.txt --output requirements-llm.txt";
+
+  scripts.ptw.exec = "poetry run ptw";
+
+  env.LD_LIBRARY_PATH = lib.mkIf pkgs.stdenv.isLinux (
+    lib.makeLibraryPath (with pkgs; [
+      gcc-unwrapped.lib
+      linuxPackages_latest.nvidia_x11
+      zlib
+      cmake
+      cudaPackages.cudatoolkit
+      cudaPackages.cudnn
+      cudaPackages.libcublas
+      cudaPackages.libcurand
+      cudaPackages.libcufft
+      cudaPackages.libcusparse
+      cudaPackages.cuda_nvtx
+      cudaPackages.cuda_cupti
+      cudaPackages.cuda_nvrtc
+      cudaPackages.nccl
+    ])
+  );
+  env.CUDA_HOME = "${pkgs.cudaPackages.cudatoolkit}";
+  env.CUDA_PATH = "${pkgs.cudaPackages.cudatoolkit}";
 
   # https://devenv.sh/pre-commit-hooks/
   # pre-commit.hooks.shellcheck.enable = true;
